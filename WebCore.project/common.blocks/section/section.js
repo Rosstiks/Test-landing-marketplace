7d5a4342ddc05b0d7ac67fabd47ea314b3322857
Rosstiks <7098750@gmail.com>
2021-10-02 20:55:05 +0300

    add second swiper

--- WebCore.project/common.blocks/section/section.js
+++ WebCore.project/common.blocks/section/section.js
@@ -1,5 +1,5 @@
 let pageWidth = window.matchMedia("(max-width: 767.98px)");
-let swiperInitiation;
+let swiperInitiation = [];
 let swiper = document.querySelectorAll(".section__items");
 let swiperWrapper = document.querySelectorAll(".section__items-wrapper");
 let swiperSlide = document.querySelectorAll(".section__item");
@@ -18,17 +18,16 @@ function checkWindowWidth() {
 }
 
 function changeViewToLayout() {
-  for (let i = 0; i < swiper.length; i++) {
-    swiper[i].classList.remove("swiper");
-  }
   for (let i = 0; i < swiperWrapper.length; i++) {
     swiperWrapper[i].classList.remove("swiper-wrapper");
   }
   for (let i = 0; i < swiperSlide.length; i++) {
     swiperSlide[i].classList.remove("swiper-slide");
   }
-
-  swiperInitiation.destroy(true, true);
+  for (let i = 0; i < swiper.length; i++) {
+    swiperInitiation[i].destroy(true, true);
+    swiper[i].classList.remove("swiper");
+  }
 }
 
 let changeSectionItems = function () {
@@ -40,55 +39,55 @@ let changeSectionItems = function () {
 };
 
 function changeViewToSlider() {
-  for (let i = 0; i < swiper.length; i++) {
-    swiper[i].classList.add("swiper");
-  }
   for (let i = 0; i < swiperWrapper.length; i++) {
     swiperWrapper[i].classList.add("swiper-wrapper");
   }
   for (let i = 0; i < swiperSlide.length; i++) {
     swiperSlide[i].classList.add("swiper-slide");
   }
+  for (let i = 0; i < swiper.length; i++) {
+    swiper[i].classList.add("swiper");
 
-  swiperInitiation = new Swiper(".swiper", {
-    loop: true,
+    swiperInitiation[i] = new Swiper(swiper[i], {
+      loop: true,
 
-    pagination: {
-      el: ".swiper-pagination",
-    },
+      pagination: {
+        el: ".swiper-pagination",
+      },
 
-    breakpoints: {
-      280: {
-        slidesPerView: 1.16,
+      breakpoints: {
+        280: {
+          slidesPerView: 1.16,
+        },
+        374: {
+          slidesPerView: 1.36,
+        },
+        424: {
+          slidesPerView: 1.56,
+        },
+        474: {
+          slidesPerView: 1.76,
+        },
+        524: {
+          slidesPerView: 1.96,
+        },
+        574: {
+          slidesPerView: 2.16,
+        },
+        624: {
+          slidesPerView: 2.36,
+        },
+        674: {
+          slidesPerView: 2.56,
+        },
+        724: {
+          slidesPerView: 2.76,
+        },
       },
-      374: {
-        slidesPerView: 1.36,
-      },
-      424: {
-        slidesPerView: 1.56,
-      },
-      474: {
-        slidesPerView: 1.76,
-      },
-      524: {
-        slidesPerView: 1.96,
-      },
-      574: {
-        slidesPerView: 2.16,
-      },
-      624: {
-        slidesPerView: 2.36,
-      },
-      674: {
-        slidesPerView: 2.56,
-      },
-      724: {
-        slidesPerView: 2.76,
-      },
-    },
 
-    spaceBetween: 20,
-    autoplay: {},
-    speed: 800,
-  });
+      spaceBetween: 20,
+      autoplay: {},
+      speed: 800,
+    });
+  }
 }
