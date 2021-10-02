8faa6280354da4d934c3c5719f681cf1c5b82142
Rosstiks <7098750@gmail.com>
2021-10-02 18:52:01 +0300

    fix

--- WebCore.project/common.blocks/button-more/button-more.js
+++ WebCore.project/common.blocks/button-more/button-more.js
@@ -41,15 +41,14 @@ function hideContent(button, element) {
 
 function showContent(button, element) {
   button.textContent = "Скрыть";
-  element.style.maxHeight = "400px";
+  element.style.maxHeight = element.scrollHeight + "px";
 }
 
 function checkMaxHeight(button, element) {
   if (
     (element.clientHeight >= element.scrollHeight &&
       !button.classList.contains("button-more--close")) ||
-    (button.classList.contains("section__button") &&
-      document.documentElement.clientWidth < 768)
+    (button.classList.contains("section__button") && window.innerWidth < 768)
   ) {
     button.style.display = "none";
   } else {
