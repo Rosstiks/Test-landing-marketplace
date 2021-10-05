0ccc4d8c3eb1c146e0954e603d6e054ecadbab9d
Rosstiks <7098750@gmail.com>
2021-10-05 14:49:34 +0300

    fix

--- WebCore.project/src/common.blocks/sidebar/sidebar.js
+++ WebCore.project/src/common.blocks/sidebar/sidebar.js
@@ -1,14 +1,24 @@
 let body = document.querySelector("body");
 let sidebar = document.querySelector(".sidebar");
+let blurArea = document.querySelector(".contant-container__blur-area");
 let buttonMenu = document.querySelectorAll(".menu-button");
 
 for (let i = 0; i < buttonMenu.length; i++) {
   changeSidebarStatus(buttonMenu[i]);
 }
 
+blurArea.addEventListener("click", function () {
+  toggleClasses();
+});
+
 function changeSidebarStatus(button) {
   button.addEventListener("click", function () {
-    sidebar.classList.toggle("sidebar--open");
-    body.classList.toggle("body-mobile-sidebar");
+    toggleClasses();
   });
+}
+
+function toggleClasses() {
+  sidebar.classList.toggle("sidebar--open");
+  blurArea.classList.toggle("contant-container__blur-area--active");
+  body.classList.toggle("body-open-sidebar");
 }
